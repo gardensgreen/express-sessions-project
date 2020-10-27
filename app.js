a7b7e6c79e50bdab1ffdaacc047b1fb301cd8a3b
Daniel TIllero <danieltillero67@gmail.com>
2020-10-27 12:45:05 -0400

    Implement session middleware and attach history to session

--- app.js
+++ app.js
@@ -1,22 +1,46 @@
-
-const express = require('express');
+const express = require("express");
+const session = require("express-session");
 
 const app = express();
 
-app.set('view engine', 'pug');
+//Middleware
+app.set("view engine", "pug");
 
-app.get('/', (req, res) => {
-  res.render('index', { title: 'Home' });
+app.use(
+    session({
+        secret: "a5d63fc5-17a5-459c-b3ba-6d81792158fc",
+        resave: false,
+        saveUninitialized: false,
+    })
+);
+
+app.use((req, res, next) => {
+    let { history } = req.session;
+
+    if (!history) {
+        history = [];
+        req.session.history = history;
+    }
+    const url = `${req.protocol}://${req.get("host")}${req.originalUrl}`;
+    history.unshift(url);
+
+    next();
 });
 
-app.get('/about', (req, res) => {
-  res.render('about', { title: 'About' });
+app.get("/", (req, res) => {
+    res.render("index", { title: "Home", history: req.session.history });
 });
 
-app.get('/contact', (req, res) => {
-  res.render('contact', { title: 'Contact' });
+app.get("/about", (req, res) => {
+    res.render("about", { title: "About", history: req.session.history });
+});
+
+app.get("/contact", (req, res) => {
+    res.render("contact", { title: "Contact", history: req.session.history });
 });
 
 const port = 8080;
 
-app.listen(port, () => console.log(`Listening for connections on port ${port}...`));
+app.listen(port, () =>
+    console.log(`Listening for connections on port ${port}...`)
+);
